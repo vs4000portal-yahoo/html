ab0b415a2db138254ac33c0253ffac3bb6e585c6
Your Name <you@example.com>
2026-09-26 02:53:43 +0000

    add routes/api.php

---
+++ routes/api.php
@@ -0,0 +1,5 @@
+use App\Models\User;
+
+Route::get('/users', function () {
+    return User::all();
+});
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/users', function () {
-    return User::all();
 });
